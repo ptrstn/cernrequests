0ec6e122486d9b75373f9d910507ef26e5eb5e8c
Peter Stein <peterstein@protonmail.com>
2020-06-08 19:38:41 +0200

    Update the cern-cacert.pem by CERN-bundle.pem
    References https://github.com/CMSTrackerDPG/cernrequests/issues/1
    
    Copied from http://linuxsoft.cern.ch/cern/centos/7/cern/x86_64/repoview/CERN-CA-certs.html

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ with open(path.join(this_directory, "README.md")) as f:
 
 setup(
     name="cernrequests",
-    version="0.3.0",
+    version="0.3.1",
     desription="CERN wrapper around the requests package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ptrstn/cernrequests",
     author="Peter Stein",
-    author_email="peter.stein@cern.ch",
+    author_email="peterstein@protonmail.com",
     packages=["cernrequests"],
     package_dir={"cernrequests": "cernrequests"},
     package_data={"cernrequests": ["*.pem"]},
